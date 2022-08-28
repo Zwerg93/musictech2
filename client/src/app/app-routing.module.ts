153cec15fc78fb2a2cf0b003db72c8e5914e5200
Zwerg93 <mapouget15160@gmail.com>
2022-08-28 08:11:58 +0200

    addded components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {PlayerComponent} from "./components/player/player.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {SearchComponent} from "./components/search/search.component";
+import {UploadComponent} from "./upload/upload.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'home'
+}, {
+  path: 'home',
+  component: HomeComponent
+},
+  {
+    path: 'player',
+    component: PlayerComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'upload',
+    component: UploadComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { PlayerComponent } from './components/player/player.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayerComponent,
+    SearchComponent,
+    ProfileComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
